7b3f4165fa67958b4271d614fc3312a7d6de3967
Eirik Vullum <evullum@gmail.com>
2014-05-31 20:31:24 +0200

    cleanup

--- dev.sh
+++ dev.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Variables set up by this script:
-# PORT                        Port exposed by this component.
-# DEBUG                       Debugging (* for all)
-# NODE_ENV                    Environment ('development', 'staging', 'production')
-# APPSECRET                   Application session secret
-# API_URL                     Client side API url
+# PORT         Port exposed by this component.
+# DEBUG        Debugging (* for all)
+# NODE_ENV     Environment ('development', 'staging', 'production')
+# APPSECRET    Application session secret
+# API_URL      Client side API url
 
 export PORT=3000 \
 export DEBUG='*' \
